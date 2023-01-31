4b020c71cfbff5d7b41fbe1293af09a2767e4944
Fransch97 <fransch345@gmail.com>
2023-02-01 00:40:14 +0100

    post get values

--- first-node-server/Functions/Functions.js
+++ first-node-server/Functions/Functions.js
@@ -6,5 +6,4 @@ const routing = (url, response) =>  {
     response.write(html);
 }
 
-
-export default routing;
+export {routing};
--- first-node-server/Server.js
+++ first-node-server/Server.js
@@ -1,13 +1,37 @@
 import http from "http" ;
-import routing from "./Functions/Functions.js";
+import {routing} from "./Functions/Functions.js";
+import fs from 'fs';
 
 const requestListener = (request, response) => {
     const url = request.url;
     
     try {
+        
+        if (url === '/message' && request.method === 'POST') {
+            const body = [];
+            const name = null;
+            
+            request.on('data', (chunk) => {
+                console.log(chunk)
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                const parsedBody = Buffer.concat(body).toString();
+                const name = parsedBody.split('=')[1]
+                fs.writeFileSync('message.txt', name);
+            });
+
+            response.statusCode = 302;
+            response.setHeader('Location', '/');
+            return response.end();
+        }
+
         routing(url, response)
+        response.end()
     } catch {
         routing('404', response)
+        response.end()
     }
 } 
 
