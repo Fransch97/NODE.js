4d590e886e8b78f1eb00e513a40debae134d3f40
Fransch97 <fransch345@gmail.com>
2023-02-06 23:17:44 +0100

    .

--- express-all/router/Routes/404.js
+++ express-all/router/Routes/404.js
@@ -3,7 +3,7 @@ import express from 'express';
 const NotFound = express.Router();
 
 NotFound.use('/', (req, res) => {
-    res.send('<h1>404</h1><h1>Route not found</h1>');
+    res.status(404).end('<h1>404</h1><h1>Route not found</h1>');
 });
 
 export default NotFound;
